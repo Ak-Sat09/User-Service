734d5f8f4cbb1e00ba449f007e9bd9a8b4d9245c
Anmol Mehla <anmolmehla1@outlook.com>
2026-02-17 15:54:42 +0530

    Updadte One

--- src/main/java/com/example/UserService/models/UsersEntity.java
+++ src/main/java/com/example/UserService/models/UsersEntity.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-//hello
+
 @Entity
 @Table(name = "users")
 @Data
